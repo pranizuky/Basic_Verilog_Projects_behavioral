484600a191d2b8fe44ead544a135c2b5407fa80a
Pranjal Upadhyay <106436202+pranizuky@users.noreply.github.com>
2024-03-11 10:26:31 +0530

    Update mux_behaviour_tb.v

--- mux_behaviour/mux_behaviour_tb.v
+++ mux_behaviour/mux_behaviour_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.03.2024 20:32:55
-// Design Name: 
-// Module Name: mux_behaviour_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module mux_behaviour_tb();
 reg [3:0]i;
